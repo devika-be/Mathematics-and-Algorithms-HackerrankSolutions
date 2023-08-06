66532d251f271e280a30e0bf3fdcccb35b69870e
Devika Pagare <65743242+devika-be@users.noreply.github.com>
2023-08-06 23:51:54 +0530

    Create ArmyGame.py

---
+++ ArmyGame.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gameWithCells' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER m
+#
+
+def gameWithCells(n, m):
+    return ((n+(n%2))*(m+(m%2))//4)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    result = gameWithCells(n, m)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
